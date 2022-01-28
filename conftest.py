748454c2ea47a2467088a00eeb01cbc8e78c65bc
dashavasyaeva <vasha_dasha_s@mail.ru>
2022-01-28 15:59:32 +0300

    update solution

--- conftest.py
+++ conftest.py
@@ -7,28 +7,22 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None, help="Choose language: ru, en ...")
 
 
-# for Chrome
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': "language"})
-# for Firefox
-fp = webdriver.FirefoxProfile()
-fp.set_preference("intl.accept_languages", "language")
-
-
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
+        print("\nstart browser chrome for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
+        print("\nstart browser firefox for test...")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        print("Browser <browser_name> still is not implemented")
-
-    user_language = request.config.getoption("language")
-
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
-    print("\nquit browser..")
+    print("\nquit browser...")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,10 +1,8 @@
-import pytest
+import time
 
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-class TestLogin:
-    def test_guest_should_see_login_link(self, browser, language):
-        link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-        browser.get(link)
-        browser.find_element_by_css_selector("#login_link")
-
+def test_guest_should_see_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    shop_button = browser.find_elements_by_css_selector(".btn.btn-lg")
+    assert len(shop_button) > 0, "Basket button not found"
